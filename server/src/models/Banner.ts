9a5f8236b37269c548240e3f72f3f0d1d484055d
yuumikunii <kuniiyuumi@gmail.com>
2022-05-26 19:01:39 -0300

    feature/banner: back-end banner

--- server/src/models/Banner.ts
+++ server/src/models/Banner.ts
@@ -1,18 +1,15 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 
 @Entity()
-export class User {
+export class Banner {
 
     @PrimaryGeneratedColumn()
     id: number
 
     @Column()
-    firstName: string
+    text: string
 
     @Column()
-    lastName: string
-
-    @Column()
-    age: number
+    description: string
 
 }
