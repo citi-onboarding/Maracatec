4e6a4c3bb9a1f3a817f925623db439802fceb269
João Pedro da Silva Nascimento <jpsn2@cin.ufpe.br>
2022-05-26 16:50:16 -0300

    CRUD of Carousel was created

---
+++ server/src/controllers/CarouselController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from 'express';
+import { Carousel } from '@models/Carousel';
+import { Citi, Crud } from '../global'
+
+export default class UserController implements Crud {
+
+    async create(request: Request, response: Response){
+        const {image, description, date} = request.body;
+
+        const isAnyUndefined = Citi.areValuesUndefined(image, description, date);
+        if(isAnyUndefined) return response.status(400).send();
+
+        const newCarousel = { image, description, date };
+        const {httpStatus, message} = await Citi.insertIntoDatabase(Carousel, newCarousel);
+
+        return response.status(httpStatus).send({ message });
+    }
+
+    async get(request: Request, response: Response){
+        const {httpStatus, values} = await Citi.getAll(Carousel);
+        return response.status(httpStatus).send(values);
+    }
+
+    async delete(request: Request, response: Response){
+        const { id } = request.params;
+        const {value: carouselFound, message } = await Citi.findByID(Carousel, id); 
+           
+        if(!carouselFound) return response.status(400).send({ message });
+
+        const {httpStatus, messageFromDelete } = await Citi.deleteValue(Carousel, carouselFound);
+        return response.status(httpStatus).send({ messageFromDelete });
+    }
+
+    async update(request: Request, response: Response){
+        const { id } = request.params;
+        const {image, description, date } = request.body;
+
+        const isAnyUndefined = Citi.areValuesUndefined(image, description, date, id);
+        if(isAnyUndefined) return response.status(400).send();
+
+        const userWithUpdatedValues = { image, description, date };
+
+        const { httpStatus, messageFromUpdate } = await Citi.updateValue(Carousel, id, userWithUpdatedValues);
+        return response.status(httpStatus).send({ messageFromUpdate });
+    }
+
+    
+}
