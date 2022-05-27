7de7c011d4fbfad2a25c3420f17a4725f9386e6f
maatheusfp <matheus.pinheirofer@gmail.com>
2022-05-27 01:14:53 -0300

    (feature/Timer): I created the Timer model as well his controller and route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,8 @@
 import express from 'express';
-import UserController from '@controllers/UserController'
 import TimerController from '@controllers/TimerController';
 
 const routes = express.Router();
-const userController = new UserController();
 const timerController = new TimerController();
-
-routes.post('/user', userController.create);
-routes.get('/user', userController.get);
-routes.delete('/user/:id', userController.delete);
-routes.put('/user/:id', userController.update);
 
 routes.post('/timer', timerController.create);
 routes.get('/timer', timerController.get);
