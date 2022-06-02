64b0213ce6d10070cf5b9b86bedd1249924e4242
camilab-vieira <82523340+camilab-vieira@users.noreply.github.com>
2022-06-02 14:49:02 -0300

    feature(ChatBox): ChatBox component created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import BannerPrincipal from './pages/BannerPrincipal';
-import { Banner2 } from './pages';
+import { Banner2, CommentBox } from './pages';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BannerPrincipal />
       <Banner2/>
+      <CommentBox/>
     </ThemeProvider>
   );
 }
---
+++ client/src/pages/CommentBox/CommentBox.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StarSymbol } from "../../assets";
+import { ChatBox } from "../../components/ChatBox";
+
+const CommentBox = () => {
+    return (
+        <div>
+            <ChatBox/>
+        </div>
+    );
+}
+
+export default CommentBox;
